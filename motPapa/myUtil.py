dd063cdc66e0c11ddb86386e63458fd3c5bcfe6c
bru08 <ppabrno@gmail,com>
2019-12-23 09:14:29 +0100

    made IoU_calc a static methods in myUtils instead of inside tracker class

--- motPapa/myUtil.py
+++ motPapa/myUtil.py
@@ -24,6 +24,24 @@ def get_bb_hw(box):
     return x, y, w, h
 
 
+def IoU_calc(boxA, boxB):
+
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute areas
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
+
+
 class DetectionFileReader:
     # TODO read in mot ch format, also have my detection in mot ch format
 
